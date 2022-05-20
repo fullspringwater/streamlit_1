7394b90f5042a0ef97cd6881bd85ae5ae09d1df5
schan <105832330+fullspringwater@users.noreply.github.com>
2022-05-20 12:53:56 +0900

    add

---
+++ app9_home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from PIL import Image
+def run_home() :
+    st.subheader('홈 화면입니다.')
+
+    st.text('파일 분리 앱 실습')
+
+    img = Image.open('data2/image_03.jpg')
+    st.image(img, use_column_width= True)
