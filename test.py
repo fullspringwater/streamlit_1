ebed18b8bc71173d108f84967cc48c24599578cc
schan <105832330+fullspringwater@users.noreply.github.com>
2022-05-23 14:15:54 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,35 @@
 import streamlit as st
-
+import pandas as pd
+import plotly.express as px
 
 def main():
+    # 스트림릿에서 제공해주는 차트
+    # line_chart, area_chart
 
+    df1 = pd.read_csv('data2/lang_data.csv')
 
-    audio_file = open('data2/song.mp3', 'rb')
-    st.audio(audio_file.read(), format='audio/mp3')
+    st.dataframe(df1)
+    
+    lang_list = df1.columns[1:]
+    choice_list = st.multiselect('언어를 선택하시오', lang_list)
 
-if __name__ == '__main__':
+    if len(choice_list) != 0:
+        df_choice = df1[choice_list]
+        st.dataframe(df_choice)
+
+        #스트림릿이 제공하는 line_chart
+        st.line_chart(df_choice)
+
+        # 스트림릿이 제고하는 area_chart
+        st.area_chart(df_choice)
+    
+    df2 = pd.read_csv('data2/iris.csv')
+    st.dataframe(df2)
+
+    #스트림릿이 제공하는 bar_chart
+    st.bar_chart(df2.iloc[:, : -2 +1 ])
+
+   
+
+if __name__=='__main__':
     main()
